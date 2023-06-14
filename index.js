86bd1a4b4d597e30845b952d99a688038da6442c
Pritesh <priteshjogdhankar@gmail.com>
2023-06-14 14:18:19 +0530

    added final css and javascript

--- index.js
+++ index.js
@@ -4,28 +4,55 @@ const clear=document.getElementById('clear')
 const range=document.getElementById('range')
 const sizeDisplay=document.getElementById('size')
 const conatiner=document.getElementById("container")
+const selectedColor=document.getElementById("color")
+let mouseDown=false;
+let color='black';
 
 function createDiv(height,width){
-    const div=document.createElement('div')
-      div.classList.add('box')
-     div.style.height=`${height}px`;
+     const div=document.createElement('div')
+     if(height===width){
+     div.classList.add('box');
+     div.style.background=`white`;
+     div.addEventListener('mousedown',()=>{
+        div.style.background=color;
+        mouseDown=true;
+
+      })
+     
+      div.addEventListener('mouseover',()=>{
+        if(mouseDown===true){
+        div.style.background=color;
+            
+        }
+      })
+      div.addEventListener('mouseup',()=>{
+            mouseDown=false;
+      })
+
+      div.addEventListener('dragstart',(event)=>{
+        event.preventDefault();
+      })
+    }
+    div.style.height=`${height}px`;
      div.style.width=`${width}px`;
-     div.style.background=`red`;
-     div.style.border=`1px solid black`;
+
      return (div)
 }
-function createGrid(gridSize){
-     deleteGrid();
-    let boxSize=500/gridSize-2;
-for(let i=0;i<gridSize;i++){
-    let rowDiv=createDiv(boxSize,500)
-    rowDiv.style.display="flex";
-   for(let j=0;j<gridSize;j++){
-        rowDiv.appendChild(createDiv(boxSize, boxSize));
-   }
-   conatiner.appendChild(rowDiv)
-}
-}
+
+  function createGrid(gridSize){
+           deleteGrid();
+       let boxSize=500/gridSize;
+       for(let i=0;i<gridSize;i++){
+
+            let rowDiv=createDiv(boxSize,500)
+             rowDiv.style.display="flex";
+             for(let j=0;j<gridSize;j++){
+                 rowDiv.appendChild(createDiv(boxSize, boxSize));
+                }
+   
+          conatiner.appendChild(rowDiv)
+        }
+    }
 
 function deleteGrid(){
     while(conatiner.firstElementChild){
@@ -35,10 +62,15 @@ function deleteGrid(){
 }
 createGrid(16)
 
-apply.addEventListener('click',()=>{
-   createGrid(range.value)
-   
-})
+conatiner.addEventListener('mouseenter',()=>{ mouseDown=false; mouseUp=true})
+
 range.addEventListener('input',()=>{
-    sizeDisplay.innerText=`${range.value}X${range.value}`
+    sizeDisplay.innerText=`${range.value}X${range.value}`;
+    createGrid(range.value)
+})
+clear.addEventListener('click',()=>{
+    createGrid(range.value)
+})
+selectedColor.addEventListener('input',()=>{
+    color=selectedColor.value
 })
